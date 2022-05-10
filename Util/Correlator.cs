1de8bd756243b7d9b072eaaf35ac969ebca9429c
Yuri <ykhrishtakyan@gmail.com>
2022-05-10 18:07:11 +0300

    change damerau-levenshtein algo to JaroWinker

--- Util/Correlator.cs
+++ Util/Correlator.cs
@@ -8,35 +8,35 @@ public class Correlator
     public void CorrelateScores(List<Pupil> pupils, List<string> transcribedElements)
     {
         //Regex getGrade = new Regex(@"[^\d]");
-        Regex getTextOnly = new Regex(@"[^А-Я\s]+");
+        var getTextOnly = new Regex(@"[^А-Я\s]+");
         foreach (var pupil in pupils)
         {
-            int minDistance = Int32.MaxValue;
-            int minPosition = 0;
-            string name = pupil.Name.ToUpper().Replace('Ё', 'Е');
-            string surname = pupil.Surname.ToUpper().Replace('Ё', 'Е');
-            for (int j = 0; j < transcribedElements.Count; j++)
+            double minDistance = 0;
+            var minPosition = 0;
+            var name = pupil.Name.ToUpper().Replace('Ё', 'Е');
+            var surname = pupil.Surname.ToUpper().Replace('Ё', 'Е');
+            for (var j = 0; j < transcribedElements.Count; j++)
             {
-                string currentTranscribedElement = transcribedElements[j].ToUpper().Replace('Ё', 'Е');
+                var currentTranscribedElement = transcribedElements[j].ToUpper().Replace('Ё', 'Е');
                 currentTranscribedElement = getTextOnly.Replace(currentTranscribedElement, "").Trim();
-                int currentNameDistance =
-                    DamerauLevenshtein.GetDistance(name, currentTranscribedElement);
-                int currentSurnameDistance =
-                    DamerauLevenshtein.GetDistance(surname, currentTranscribedElement);
-                int totalDistance =
-                    DamerauLevenshtein.GetDistance(surname + " " + name, currentTranscribedElement);
-                int minDistanceBetween = currentNameDistance < currentSurnameDistance
-                    ? currentNameDistance < totalDistance ? currentNameDistance : totalDistance
-                    : currentSurnameDistance < totalDistance ? currentSurnameDistance : totalDistance;
-                if (minDistanceBetween < minDistance)
+                var currentNameDistance =
+                    JaroWinklerDistance.GetDistance(name, currentTranscribedElement);
+                var currentSurnameDistance =
+                    JaroWinklerDistance.GetDistance(surname, currentTranscribedElement);
+                var totalDistance =
+                    JaroWinklerDistance.GetDistance(surname + " " + name, currentTranscribedElement);
+                var minDistanceBetween = currentNameDistance > currentSurnameDistance
+                    ? currentNameDistance > totalDistance ? currentNameDistance : totalDistance
+                    : currentSurnameDistance > totalDistance ? currentSurnameDistance : totalDistance;
+                if (minDistanceBetween > minDistance)
                 {
                     minDistance = minDistanceBetween;
                     minPosition = j;
                 }
             }
 
-            pupil.Note = transcribedElements[minPosition].Substring(transcribedElements[minPosition].LastIndexOf(" ")).Trim();
-            transcribedElements.RemoveAt(minPosition);
+            pupil.Note = transcribedElements[minPosition][transcribedElements[minPosition].LastIndexOf(" ")..].Trim();
+            //transcribedElements.RemoveAt(minPosition);
         }
     }
 }
---
+++ Util/JaroWinkler.cs
@@ -0,0 +1,12 @@
+﻿using SimMetrics.Net.Metric;
+
+namespace VoiceGradeApi.Util;
+
+public static class JaroWinklerDistance
+{
+    public static double GetDistance(string original, string modified)
+    {
+        var jaroWinkler = new JaroWinkler();
+        return jaroWinkler.GetSimilarity(original, modified);
+    }
+}
